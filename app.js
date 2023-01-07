16c45796c8ea785271f041c5981fabcf75b8fc0d
walkindallas <matkovukovickif@gmail.com>
2023-01-07 16:34:13 +0100

    commit 2 - added login page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
-const homeStartingContent = "Home page is place where I loggedd all my progress in web development. It's structured like journal with entries on day to day basis.";
+const homeStartingContent = "Home page is place where I logged all my progress in web development. It's structured like journal with entries on day to day basis.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -24,6 +24,20 @@ const postSchema = {
 
 const Post = mongoose.model("Post", postSchema);
 
+const userSchema = ({
+  email: String,
+  password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+const newUser = new User ({
+  email: "mvuk@test.com",
+  password: "test1234"
+})
+
+// newUser.save()
+
 app.get("/", function(req, res){
 
   Post.find({}, function(err, posts){
@@ -34,9 +48,6 @@ app.get("/", function(req, res){
   });
 });
 
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
 
 app.post("/compose", function(req, res){
   const post = new Post({
@@ -72,6 +83,28 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
+
+app.get("/login", function(req, res){
+  res.render("login")
+})
+
+app.post("/login", function(req, res){
+  const username = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("compose");
+        }
+      }
+    }
+  });
+});
+
 
 
 app.listen(3000, function() {
